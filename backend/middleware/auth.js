085f51792c0ac4f2daffc987b137fafe0d06ad67
Mohak Ahuja <mohakaahuaj@outlook.com>
2023-02-26 11:53:10 +0530

    changes in middleware/auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,12 +1,28 @@
 const ErrorHandler = require("../utils/errorhandler");
 
-exports.isAuthenticated=(req,res,next) => {
-    const token = req.cookies["connect.sid"];
-    if (!token) {
-      return next(new ErrorHandler("Not Logged In", 401));
-    }
-    next();
-  };
+// exports.isAuthenticated=(req,res,next) => {
+//     const token = req.cookies["connect.sid"];
+//     if (!token) {
+//       return next(new ErrorHandler("Not Logged In", 401));
+//     }
+//     next();
+//   };
+exports.isAuthenticated = (req, res, next) => {
+  const token = req.cookies["connect.sid"];
+  if (!token) {
+    // if the token doesn't exist, the user is not authenticated
+    return next(new ErrorHandler("Not Logged In", 401));
+  }
+  // if the token exists, check if the user is authenticated with Passport
+  if (req.isAuthenticated()) {
+    // user is authenticated, continue with the next middleware
+    return next();
+  } else {
+    // user is not authenticated, redirect to the homepage or other appropriate URL
+    res.redirect(process.env.FRONTEND_URL);
+  }
+};
+
 
   // importing in orderRoutes
   exports.authorizeAdmin = (req, res, next) => {
