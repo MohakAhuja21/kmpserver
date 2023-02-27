937a001d9b86f37bed916e3cba1fc2a9219bbe88
Mohak Ahuja <mohakaahuaj@outlook.com>
2023-02-27 16:16:59 +0530

    c2

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -7,14 +7,7 @@ exports.myProfile = (req, res) => {
   res.status(200).json({
     success: true,
     user: req.user,
-    message: "Logged in successfully",
   });
-  // if (req.user) {
-  //   res.status(200).json({ user: req.user, success: true });
-  // }
-  // else {
-  //   res.status(401).json({ message: "Not Authorized" });
-  // }
 };
 
 exports.logout = (req, res, next) => {
